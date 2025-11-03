dfb0a2f06f2284dcadf87e2f9300731ee41dab8f
OscarValle <oscar.valle@hotmail.es>
2025-11-03 21:16:43 +0100

    Rental Basic Repository Added
    Rental Basic Repository Added

---
+++ source/repo3/hexagonal-architecture-doc-main/src/GtMotive.Estimate.Microservice.ApplicationCore/UseCases/Rentals/IRentalRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GtMotive.Estimate.Microservice.Domain.Entities;
+
+namespace GtMotive.Estimate.Microservice.ApplicationCore.UseCases.Rentals
+{
+    /// <summary>
+    /// Intercafe to RentalRepository.
+    /// </summary>
+    public interface IRentalRepository
+    {
+        /// <summary>
+        /// Add Rental.
+        /// </summary>
+        /// <param name="rental">Rental.</param>
+        /// <returns>Task.</returns>
+        Task AddAsync(Rental rental);
+
+        /// <summary>
+        /// Ends or updates an existing rental.
+        /// </summary>
+        /// <param name="rental">Rental updated with return details.</param>
+        /// <returns>Task.</returns>
+        Task UpdateAsync(Rental rental);
+
+        /// <summary>
+        /// Get all Customer Rentals.
+        /// </summary>
+        /// <param name="customerId">Is Available.</param>
+        /// <param name="isActive">Is Active.</param>
+        /// <returns>IEnumerable of all Available Rentals.</returns>
+        Task<IEnumerable<Rental>> GetByCustomerIdAsync(string customerId, bool? isActive);
+
+        /// <summary>
+        /// Get a Rental by Id.
+        /// </summary>
+        /// <param name="id">Rental Id.</param>
+        /// <returns>A Rental.</returns>
+        Task<Rental> GetByIdAsync(Guid id);
+    }
+}
---
+++ source/repo3/hexagonal-architecture-doc-main/src/GtMotive.Estimate.Microservice.Infrastructure/MongoDb/Repositories/RentalRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GtMotive.Estimate.Microservice.ApplicationCore.UseCases.Rentals;
+using GtMotive.Estimate.Microservice.Domain.Entities;
+using GtMotive.Estimate.Microservice.Infrastructure.MongoDb.Settings;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace GtMotive.Estimate.Microservice.Infrastructure.MongoDb.Repositories
+{
+    public class RentalRepository : IRentalRepository
+    {
+        private readonly IMongoCollection<Rental> _rentalsCollection;
+
+        public RentalRepository(MongoService mongoService, IOptions<MongoDbSettings> options)
+        {
+            ArgumentNullException.ThrowIfNull(mongoService);
+            ArgumentNullException.ThrowIfNull(options);
+
+            var database = mongoService.MongoClient.GetDatabase(options.Value.MongoDbDatabaseName);
+
+            _rentalsCollection = database.GetCollection<Rental>("Rentals");
+        }
+
+        public Task AddAsync(Rental rental) => _rentalsCollection.InsertOneAsync(rental);
+
+        public Task UpdateAsync(Rental rental) => _rentalsCollection.ReplaceOneAsync(r => r.Id == rental.Id, rental);
+
+        public async Task<IEnumerable<Rental>> GetByCustomerIdAsync(string customerId, bool? isActive)
+        {
+            var filter = Builders<Rental>.Filter.Eq(r => r.CustomerId, customerId);
+
+            if (isActive.HasValue)
+            {
+                var isActiveFilter = Builders<Rental>.Filter.Eq(r => r.IsActive, isActive.Value);
+
+                filter = Builders<Rental>.Filter.And(filter, isActiveFilter);
+            }
+
+            return await _rentalsCollection.Find(filter).ToListAsync();
+        }
+
+        public async Task<Rental> GetByIdAsync(Guid id)
+        {
+            return await _rentalsCollection.Find(v => v.Id == id).FirstOrDefaultAsync();
+        }
+    }
+}
